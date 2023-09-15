db764c5507b6ff499d3e3e7bc3885ad65b1d8f6b
FigoHunter <zhaoyifei19950308@hotmail.com>
2023-09-15 17:50:41 +0800

    process

--- Assets/Editor/SetupObject.cs
+++ Assets/Editor/SetupObject.cs
@@ -4,16 +4,78 @@ using UnityEngine;
 using UnityEditor;
 using Figo;
 using System.IO;
+using UnityEditor.SceneManagement;
+using System.Linq;
+using Codice.CM.Client.Differences.Merge;
+
 public class SetupObject:AssetPostprocessor
 {
+    public const int SimulationFPS = 120;
+    public const float SimTime = 2f;
     public static string OBJ_ASSET_PATH => Path.Combine("Assets", "Res","Decimated");
-
+    public const string SCENE_PATH = "Assets/Scenes/SampleScene.unity";
 
     void OnPreprocessModel()
     {
         ModelImporter modelImporter = assetImporter as ModelImporter;
         modelImporter.useFileScale = false;
         modelImporter.globalScale = 0.01f;
+    }
+
+    [MenuItem("Rosita/Physical Alignment/Executing_Process")]
+    public static void Execute_Main()
+    {
+        EditorSceneManager.OpenScene(SCENE_PATH, OpenSceneMode.Single);
+#if UNITY_EDITOR_WIN
+        var sep = ';';
+#elif UNITY_EDITOR_LINUX
+        var sep = ':';
+#else
+        throw new System.Exception("Platform Not Valid");
+#endif
+        //if(Application.platform)
+        var args = System.Environment.GetEnvironmentVariable("ROSITA_OBJS");
+        List<string> objs;
+        if (!string.IsNullOrEmpty(args))
+        {
+            objs = args.Split(sep).ToList();
+        }
+        else
+        {
+            if (Application.isBatchMode)
+            {
+                return;
+            }
+            objs = new List<string>();
+            var path = FigoEditorUtility.OpenFolderPanel("Load Obj", "", "");
+            foreach (var o in FigoPath.WalkThrough(path, true))
+            {
+                var p = Path.Combine(path, o);
+                if (p.EndsWith(".obj"))
+                {
+                    objs.Add(Path.GetFullPath(p).ForwardSlash());
+                }
+            }
+        }    
+        ClearResPath();
+        var assets = ImportObj(objs.Where(p => p.EndsWith(".obj")).ToArray());
+
+        var gos = InstantiateObjs(assets);
+        Vector3 currentPos = Vector3.zero;
+
+        foreach (var go in gos)
+        {
+            Debug.Log(go);
+            var physicalAlignment = SetupAlignment(go);
+            currentPos += new Vector3(0f, 0f, physicalAlignment.MaxDimension) * 3f;
+            physicalAlignment.transform.position = currentPos;
+            currentPos += new Vector3(0f, 0f, physicalAlignment.MaxDimension) * 3f;
+        }
+
+        var phyAligns = gos.Select(x => x.GetComponent<PhysicalAlignment>());
+
+        EditorPlay(phyAligns.ToArray());
+        DumpJson(phyAligns.ToArray());
     }
 
     [MenuItem("Rosita/Physical Alignment/Import Obj")]
@@ -37,21 +99,35 @@ public class SetupObject:AssetPostprocessor
         var os = Object.FindObjectsOfType<PhysicalAlignment>();
         foreach (var o in os)
         {
-            Object.DestroyImmediate(o);
+            Object.DestroyImmediate(o.gameObject);
         }
 
-        var assets = FigoAsset.FindAssets<GameObject>("t:GameObject", "Assets/Res");
+        List<PhysicalAlignment> phyAligns = new List<PhysicalAlignment>();
+
+        var assets = FigoAsset.FindAssets<GameObject>("t:GameObject", "Assets/Res/Decimated");
+        assets = assets.Where(a=>a.GetComponent<PhysicalAlignment>()!=null).ToArray();
         var gos = InstantiateObjs(assets);
+        Vector3 currentPos = Vector3.zero;
         foreach (var go in gos)
         {
-            SetupAlignment(go);
+            var physicalAlignment = SetupAlignment(go);
+            currentPos += new Vector3(0f, 0f, physicalAlignment.MaxDimension)*3f;
+            physicalAlignment.transform.position = currentPos;
+            currentPos += new Vector3(0f, 0f, physicalAlignment.MaxDimension)*3f;
+            phyAligns.Add(physicalAlignment);
         }
+    }
+
+    [MenuItem("Rosita/Physical Alignment/Reload Scene")]
+    public static void Menu_ReloadScene()
+    {
+        EditorSceneManager.OpenScene(SCENE_PATH, OpenSceneMode.Single);
     }
 
     [MenuItem("GameObject/Rosita/Physical Alignment/Move To Plane")]
     public static void Menu_MoveToPlane()
     {
-        GetSelected().GetOrAddComponent<PhysicalAlignment>().MoveToPlane(Quaternion.identity);
+        GetSelected().GetOrAddComponent<PhysicalAlignment>().SetupAlignment();
     }
 
     private static DiceFace diceFace = DiceFace.ONE;
@@ -62,7 +138,7 @@ public class SetupObject:AssetPostprocessor
         diceFace = (DiceFace)(((int)diceFace+1)%6);
         var rot = diceFace.RollDice();
         selected.transform.rotation = rot;
-        GetSelected().GetOrAddComponent<PhysicalAlignment>().MoveToPlane(rot);
+        GetSelected().GetOrAddComponent<PhysicalAlignment>().SetupAlignment();
     }
 
     [MenuItem("GameObject/Rosita/Physical Alignment/Setup Rigidbody")]
@@ -75,25 +151,54 @@ public class SetupObject:AssetPostprocessor
     [MenuItem("Rosita/Physical Alignment/Play")]
     public static void Menu_Play()
     {
-        EditorApplication.EnterPlaymode();
+        var phyAligns = Object.FindObjectsOfType<PhysicalAlignment>();
+        EditorPlay(phyAligns);
+    }
+
+    public static void EditorPlay(params PhysicalAlignment[] phyAligns)
+    {
+        Physics.autoSimulation = false;
+        var step = 1f / SimulationFPS;
+        foreach (var pa in phyAligns)
+        {
+            pa.Start();
+        }
+        for (int j = 0; j < SimTime * SimulationFPS; j++)
+        {
+            foreach (var pa in phyAligns)
+            {
+                pa.FixedUpdate();
+            }
+            Physics.Simulate(step);
+            foreach (var pa in phyAligns)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    (var pos, var rot) = pa.GetDelta((DiceFace)i);
+                }
+            }
+        }
     }
 
 
-
-    public static (PhysicalAlignment pa, Rigidbody rb, MeshCollider mc) SetupAlignment(GameObject go)
+    public static PhysicalAlignment SetupAlignment(GameObject go)
     {
-        var rigidbody = go.GetOrAddComponent<Rigidbody>();
-        var collider = go.GetOrAddComponent<MeshCollider>();
-        var mesh = go.GetComponentInChildren<MeshFilter>().sharedMesh;
-        collider.sharedMesh = mesh;
-        collider.convex = true;
+        //var rigidbody = go.GetOrAddComponent<Rigidbody>();
+        //var collider = go.GetOrAddComponent<MeshCollider>();
+        //var mesh = go.GetComponentInChildren<MeshFilter>().sharedMesh;
+        //collider.sharedMesh = mesh;
+        //collider.convex = true;
+
         var physicalAlignment = go.GetOrAddComponent<PhysicalAlignment>();
-        return (physicalAlignment, rigidbody, collider);
+        physicalAlignment.SetupAlignment();
+        return physicalAlignment;
     }
 
     public static GameObject[] ImportObj(params string[] path)
     {
         Debug.Log(ApplicationPath.projectPath);
+
+        string[] assetPath = new string[path.Length];
 
         for (int i = 0; i < path.Length; i++)
         {
@@ -102,15 +207,25 @@ public class SetupObject:AssetPostprocessor
             var baseName = ps[ps.Length-3] + ".obj";
             var target = Path.Combine(ApplicationPath.projectPath, OBJ_ASSET_PATH, baseName);
             File.Copy(p, target);
-            path[i] = Path.Combine(OBJ_ASSET_PATH, baseName);
+            assetPath[i] = Path.Combine(OBJ_ASSET_PATH, baseName);
         }
         List<GameObject> result = new List<GameObject>();
-        foreach (var p in path)
+
+        for (int i = 0; i < assetPath.Length; i++)
         {
+            var p = assetPath[i];
             AssetDatabase.ImportAsset(p);
             var go = AssetDatabase.LoadAssetAtPath<GameObject>(p);
+            var newPath = p.Substring(0, p.Length - ".obj".Length)+".prefab";
+            var newGo = Object.Instantiate(go);
+            newGo.name = go.name;
+            var comp = newGo.GetOrAddComponent<PhysicalAlignment>();
+            comp.Path = path[i];
+            go = PrefabUtility.SaveAsPrefabAsset(newGo, newPath);
+            Object.DestroyImmediate(newGo);
             result.Add(go);
         }
+        AssetDatabase.Refresh();
         return result.ToArray();
     }
 
@@ -135,6 +250,28 @@ public class SetupObject:AssetPostprocessor
         return instantiated;
     }
 
+    public static PhysicalAlignment[] InstantiateObjs(params PhysicalAlignment[] assets)
+    {
+        PhysicalAlignment[] instantiated = new PhysicalAlignment[assets.Length];
+        for (int i = 0; i < assets.Length; i++)
+        {
+            instantiated[i] = Object.Instantiate(assets[i].gameObject).GetComponent<PhysicalAlignment>();
+            instantiated[i].gameObject.name = assets[i].gameObject.name;
+        }
+        return instantiated;
+    }
+
+
+    public static void DumpJson(params PhysicalAlignment[] phyAligns)
+    {
+        foreach (var pa in phyAligns)
+        {
+            var result = pa.GetFinalJson();
+            var targetP = Path.ChangeExtension(pa.Path, "json");
+            File.WriteAllText(targetP, result);
+        }
+    }
+
     private static GameObject GetSelected()
     {
         GameObject selected = Selection.activeObject as GameObject;
@@ -144,5 +281,6 @@ public class SetupObject:AssetPostprocessor
         }
         return selected;
     }
+
 
 }
--- Assets/Scripts/PhysicalAlignment.cs
+++ Assets/Scripts/PhysicalAlignment.cs
@@ -1,47 +1,273 @@
+using Figo;
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AlignmentState:int
+{
+    Not_Stable,
+    Concatenate,
+    Not_Concatenate
+}
 
+[System.Serializable]
+public class AlignmentData
+{
+    [System.Serializable]
+    public struct ConcatenateInfo
+    {
+        public float[] pos;
+        public float[] rot;
+    }
+    public List<ConcatenateInfo> data = new List<ConcatenateInfo>();
+
+    public void Append(Vector3 pos, Quaternion rot)
+    {
+        var piece = new ConcatenateInfo()
+        {
+            pos = new float[] { pos.x, pos.y, pos.z },
+            rot = new float[] { rot.w, rot.x, rot.y, rot.z }
+        };
+        data.Add(piece);
+    }
+}
 
 public class PhysicalAlignment : MonoBehaviour
 {
+    public const float DeltaThreshold = 10e-5f;
+    public const float DeltaRotThreshold = 10e-5f;
+    public const float FinalRotThreshold = 25f;
+
     [SerializeField]
-    Bounds? m_Bounds;
+    private Bounds m_Bounds;
 
     public Bounds Bounds
     {
         get
         {
-            if (!m_Bounds.HasValue)
+            if (m_Bounds.extents.sqrMagnitude < 10e-5)
             {
                 Mesh mesh = GetComponentInChildren<MeshFilter>().sharedMesh;
                 mesh.RecalculateBounds();
                 m_Bounds = mesh.bounds;
             }
-            return m_Bounds.Value;
+            return m_Bounds;
         }
     }
 
+    [SerializeField]
+    private string m_Path;
+    public string Path { get => m_Path; set => m_Path = value; }
 
-    public void MoveToPlane(DiceFace diceFace)
+    [SerializeField]
+    private GameObject[] m_FaceObjects = new GameObject[6];
+
+    [SerializeField]
+    private float m_MaxDimension;
+
+    public float MaxDimension
     {
-        var rot = diceFace.RollDice();
-        Vector3 pos = transform.position;
-        var max = rot * Bounds.max;
-        var min = rot * Bounds.min;
-
-        transform.position = new Vector3(pos.x, -Mathf.Min(max.y, min.y), pos.z);
-        transform.rotation = rot;
+        get
+        {
+            if (m_MaxDimension < 10e-5)
+            {
+                m_MaxDimension = Mathf.Max(Bounds.max.x, Bounds.max.y, Bounds.max.z);
+            }
+            return m_MaxDimension;
+        }
     }
 
-    public void MoveToPlane(Quaternion rot)
+    private Matrix4x4[] m_Delta = new Matrix4x4[6];
+
+    [SerializeField]
+    private float[] m_DeltaPos = new float[6];
+    [SerializeField]
+    private float[] m_DeltaRot = new float[6];
+
+    [SerializeField]
+    private Matrix4x4[] m_DeltaOverall = new Matrix4x4[6];
+
+    [SerializeField]
+    private float[] m_FinalTranslation = new float[6];
+    [SerializeField]
+    private float[] m_FinalRotation = new float[6];
+
+    [SerializeField]
+    private AlignmentState[] m_AlignmentStates = new AlignmentState[6];
+
+    [SerializeField]
+    private Matrix4x4[] mats = new Matrix4x4[6];
+
+    public GameObject GetGameObject(DiceFace face)
     {
-        Vector3 pos = transform.position;
+        return m_FaceObjects[(int)face];
+    }
+    public (float deltaPos, float deltaRot) GetDelta(DiceFace face)
+    {
+        (var pos, var rot, _) = m_Delta[(int)face].GetTrs();
+        rot.ToAngleAxis(out var angle, out _);
+        return (pos.magnitude, angle);
+    }
+
+    public (float deltaPos, float deltaRot) GetDeltaOverall(DiceFace face)
+    {
+        (var pos, var rot, _) = m_DeltaOverall[(int)face].GetTrs();
+
+        rot.ToAngleAxis(out var angle, out _);
+
+        return (pos.magnitude, angle);
+    }
+
+    public (Vector3 deltaPos, Quaternion deltaRot) GetDeltaTrOverall(DiceFace face)
+    {
+        (var pos, var rot, _) = m_DeltaOverall[(int)face].GetTrs();
+        return (pos, rot);
+    }
+
+    public AlignmentState GetAlignmentState(DiceFace face)
+    {
+        return m_AlignmentStates[(int)face];
+    }
+
+
+    public void MoveToPlane(Transform t, DiceFace diceFace)
+    {
+        var rot = diceFace.RollDice();
+        Vector3 pos = t.position;
         var max = rot * Bounds.max;
         var min = rot * Bounds.min;
 
-        transform.position = new Vector3(pos.x, -Mathf.Min(max.y, min.y), pos.z);
-        transform.rotation = rot;
+        t.position = new Vector3(pos.x, -Mathf.Min(max.y, min.y), pos.z);
+        t.rotation = rot;
+    }
+
+    public void MoveToPlane(Transform t, Quaternion rot)
+    {
+        Vector3 pos = t.position;
+        var max = rot * Bounds.max;
+        var min = rot * Bounds.min;
+
+        t.position = new Vector3(pos.x, -Mathf.Min(max.y, min.y), pos.z);
+        t.rotation = rot;
+    }
+
+    public void SetupAlignment()
+    {
+        var meshObject = gameObject.GetComponentInChildren<MeshFilter>().gameObject;
+        var mesh = gameObject.GetComponentInChildren<MeshFilter>().sharedMesh;
+
+        var rigidbody = meshObject.GetOrAddComponent<Rigidbody>();
+        var collider = meshObject.GetOrAddComponent<MeshCollider>();
+        collider.sharedMesh = mesh;
+        collider.convex = true;
+        var meshObjName = meshObject.name;
+        meshObject.name = meshObjName + "_ONE";
+        m_FaceObjects[(int)DiceFace.ONE] = meshObject;
+
+        Vector3 currentPos = transform.position;
+        meshObject.transform.position = currentPos;
+        MoveToPlane(meshObject.transform, DiceFace.ONE);
+        //mats[0] = meshObject.transform.localToWorldMatrix;
+        m_DeltaOverall[0] = Matrix4x4.identity;
+
+        for (int i = 0; i < 5; i++)
+        {
+            currentPos += Vector3.right * 6f * MaxDimension;
+            var face = (DiceFace)i + 1;
+            var obj = Object.Instantiate(meshObject, transform);
+            obj.name = meshObjName + "_" + face;
+            obj.transform.position = currentPos;
+            MoveToPlane(obj.transform, face);
+            m_FaceObjects[(int)face] = obj;
+            //mats[i + 1] = obj.transform.localToWorldMatrix;
+            m_DeltaOverall[i + 1] = Matrix4x4.identity;
+        }
+    }
+
+    public void Start()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            var obj = GetGameObject((DiceFace)i);
+            mats[i] = obj.transform.localToWorldMatrix;
+            m_AlignmentStates[i] = AlignmentState.Not_Stable;
+        }
+    }
+
+    public Vector3[] pre = new Vector3[6];
+    public void FixedUpdate()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            var preM = mats[i] * m_DeltaOverall[i];
+            (pre[i],_,_) = preM.GetTrs();
+            var delta = Matrix4x4.Inverse(preM) * GetGameObject((DiceFace)i).transform.localToWorldMatrix;
+            m_DeltaOverall[i] = Matrix4x4.Inverse(mats[i]) * GetGameObject((DiceFace)i).transform.localToWorldMatrix;
+            m_Delta[i] = delta;
+            (m_FinalTranslation[i], m_FinalRotation[i]) = GetDeltaOverall((DiceFace)i);
+            (m_DeltaPos[i], m_DeltaRot[i]) = GetDelta((DiceFace)i);
+
+            if (m_DeltaPos[i] < DeltaThreshold && m_DeltaRot[i] < DeltaRotThreshold)
+            {
+                if (m_FinalRotation[i] < FinalRotThreshold)
+                {
+                    m_AlignmentStates[i] = AlignmentState.Concatenate;
+                }
+                else
+                {
+                    m_AlignmentStates[i] = AlignmentState.Not_Concatenate;
+                }
+            }
+            else
+            {
+                m_AlignmentStates[i] = AlignmentState.Not_Stable;
+            }
+        }
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    public static void Init()
+    {
+        Physics.autoSimulation = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            switch (GetAlignmentState((DiceFace)i))
+            {
+                case AlignmentState.Not_Stable:
+                    Gizmos.color = Color.yellow;
+                    break;
+                case AlignmentState.Not_Concatenate:
+                    Gizmos.color = Color.red;
+                    break;
+                case AlignmentState.Concatenate:
+                    Gizmos.color = Color.green;
+                    break;
+            }
+            var go = GetGameObject((DiceFace)i);
+            Mesh mesh = go.GetComponentInChildren<MeshFilter>().sharedMesh;
+            Gizmos.matrix = go.transform.localToWorldMatrix;
+            Gizmos.DrawWireMesh(mesh);
+        }
+    }
+
+    public string GetFinalJson()
+    {
+        AlignmentData data = new AlignmentData();
+        for (int i = 0; i < 6; i++)
+        {
+            var state = GetAlignmentState((DiceFace)i);
+            if (state == AlignmentState.Concatenate)
+            {
+                (var pos, var rot) = GetDeltaTrOverall((DiceFace)i);
+                pos = pos - Vector3.up*0.1f;
+                data.Append(pos, rot);
+            }
+        }
+        return JsonUtility.ToJson(data);
     }
 }
